e6e5e18d0ee9114a1b11d09da774cab2eb0ccf69
ganlvtech <ganlvtech@qq.com>
2017-07-06 05:38:23 +0800

    新增：移动端滑动手势打开关闭菜单

--- public/static/mobile/js/mobile.js
+++ public/static/mobile/js/mobile.js
@@ -1,15 +1,19 @@
 (function () {
+    var $sidebar = $('.sidebar');
+
     function showSidebar() {
-        $('.sidebar').show();
+        $sidebar.show();
         setTimeout(function () {
-            $('.sidebar').addClass('active');
+            $sidebar.addClass('active');
         }, 0);
     }
 
     function hideSidebar() {
-        $('.sidebar').removeClass('active');
+        $sidebar.removeClass('active');
         setTimeout(function () {
-            $('.sidebar').hide();
+            if (!$sidebar.hasClass('active')) {
+                $sidebar.hide();
+            }
         }, 500);
     }
 
@@ -19,12 +23,25 @@
     $('.sidebar-bg').click(function () {
         hideSidebar();
     });
-    $(document).pjax('a[data-pjax]', '.main', {});
+    $(document).pjax('a[data-pjax]', '.main');
     $(document).on('pjax:clicked', function () {
         hideSidebar();
     });
     $(document).on('pjax:end', function () {
         $("img[data-original]").lazyload();
+    });
+
+    $("body").swipe({
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            var $sidebar = $('.sidebar');
+            if (direction === 'left' && !$sidebar.hasClass('active')) {
+                showSidebar();
+            } else if (direction === 'right' && $sidebar.hasClass('active')) {
+                hideSidebar();
+            }
+        },
+        threshold: 50,
+        fingers: 'all'
     });
 
     /* only for index */
