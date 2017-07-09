0ed74f3351d33fd5af7d85313db00cff15f68d16
ganlvtech <ganlvtech@qq.com>
2017-07-09 21:34:26 +0800

    修改移动端样式

--- application/mobile/controller/Index.php
+++ application/mobile/controller/Index.php
@@ -9,13 +9,13 @@ class Index
     public function index()
     {
         $content = view()->getContent();
-        return $this->viewPjaxOrRead(compact('content'));
+        return $this->viewPjaxOrRead(compact('content'), true);
     }
 
     public function colleges()
     {
         $content = view()->getContent();
-        return $this->viewPjaxOrRead(compact('content'));
+        return $this->viewPjaxOrRead(compact('content'), true);
     }
 
     public function read($id)
@@ -28,11 +28,12 @@ class Index
         $content = preg_replace_callback('/<img.*?>/', function ($matches) {
             return preg_replace('/ src="(.+?)"/', ' data-original="$1"', $matches[0]);
         }, $content);
-        return $this->viewPjaxOrRead(compact('content'));
+        return $this->viewPjaxOrRead(compact('content'), false);
     }
 
-    public function viewPjaxOrRead($data)
+    public function viewPjaxOrRead($data, $is_raw = false)
     {
+        $data['is_raw'] = $is_raw;
         if (request()->isPjax()) {
             return view('pjax', $data);
         }
